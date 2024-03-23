df75144bc1e92f9852d8e15d0d3e8642d3cf9ffa
fatimazahra <businesssoumia66@gmail.com>
2024-03-23 08:45:46 +0000

    Second commit

--- WebForm1/obj/Debug/net8.0/WebForm1.AssemblyInfo.cs
+++ WebForm1/obj/Debug/net8.0/WebForm1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WebForm")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WebForm1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd44ba538d5ec801b26e0fa68958c0e62be535ca")]
-[assembly: System.Reflection.AssemblyProductAttribute("WebForm")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WebForm")]
+[assembly: System.Reflection.AssemblyProductAttribute("WebForm1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WebForm1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
